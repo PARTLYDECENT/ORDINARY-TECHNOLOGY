d919866f808f126aaa6f512374289ccfbd876dcc
PARTLYDECENT <chainsawsversuslions@gmail.com>
2025-04-02 03:14:29 -0400

    Update and rename terrain.html to mental.js

---
+++ mental.js
@@ -0,0 +1,885 @@
+## WARNING: THIS CODE IS A NIGHTMARE.
+## A 200-line mess showcasing the worst programming languages ever.
+
+# Encrypted Header (Base64 encoded nonsense)
+# bW9uc3Ryb3NpdHkgYWN0aXZhdGVkIQo=
+
+# INTERCAL (Pretend we're doing something polite)
+"PLEASE DO ,1 <- #13"
+"DO ,1 SUB #1 <- #234"
+
+# Brainfuck (Just to print 'A')
+"++++++++[>++++++++<-]>+.
+"
+
+# Malbolge (This line will summon demons if executed)
+"('&%:9]!~}|z2Vx7vl"
+
+# Cow (Because why not?)
+"MOo MoO moo Moo MOO"
+
+# Shakespeare (A play where Hamlet does math)
+"Hamlet: You are as sweet as the sum of yourself and Ophelia!"
+
+# Piet (Imagine a pixel-art program hidden here)
+"(This part is a visual representation of suffering.)"
+
+# Whitespace (Invisible operations happening)
+"(literally just spaces and tabs doing things secretly)"
+
+# LOLCODE (For the memes)
+"HAI 1.2
+  CAN HAS STDIO?
+  VISIBLE """O HAI WORLD!"""
+KTHXBYE"
+
+# Let's add more chaos...
+
+
+
+
+
+
+/* WARNING: You are not supposed to understand this shader. */
+
+// Encrypted Header (Base64 encoded nonsense)
+// VGhpcyBpcyBhIGN1cnNlZCBzaGFkZXIuIEVudGVyIGF0IHlvdXIgb3duIHJpc2su
+
+// ???
+void main() {
+    vec2 uv = gl_FragCoord.xy / vec2(800.0, 600.0);
+    float timeFactor = mod(time * 0.1, 6.28);
+    
+    vec3 color = vec3(sin(uv.x * 10.0 + timeFactor), cos(uv.y * 10.0 - timeFactor), sin(timeFactor));
+    
+    gl_FragColor = vec4(color, 1.0);
+}
+
+// ???
+
+
+
+
+/////////////////////////////otsf.js/////////////////////////////////////
+
+
+
+
+
+
+
+# INTERCAL - Just being extra polite
+"PLEASE DO ,2 <- #42"
+"DO ,2 SUB #3 <- #99"
+
+# Brainfuck - Making a loop that does nothing useful
+"[->+<]>>++++++++[<++++++++>-]<."
+
+# Malbolge - Still making no sense
+"98v6{*l:!}bq@"
+
+# Cow - More moos for no reason
+"moo MOO MoO moO MOo"
+
+# Shakespeare - Now a tragic monologue
+"Ophelia: Thou art as annoying as the sum of Hamlet and a brick."
+
+# Piet - Probably more abstract chaos
+"(Imagine more random color-coded pixels here)"
+
+# Whitespace - Still lurking invisibly
+"(More invisible madness here)"
+
+# LOLCODE - Making it talk more nonsense
+"HAI 1.2
+  CAN HAS STDIO?
+  I HAS A VAR ITZ 42
+  VISIBLE """I HAS A NUMBER!"""
+KTHXBYE"
+
+# Extra Brainfuck - Just filling space with confusion
+"+++++[->++<]>.
+"
+
+# More Malbolge - This should never be executed
+"$%&@!9~{+^&}z"
+
+# Extra Cow - Because we're milking this joke
+"Moo MOo moO MOO moo MoO"
+
+# Even more Shakespeare - Because drama
+"Hamlet: To debug or not to debug, that is the question."
+
+# Whitespace - The ghost code keeps haunting us
+"(Yet more invisible operations happening)"
+
+# Piet - More unreadable graphical programming
+"(Insert more abstract art here)"
+
+# More LOLCODE - Just to reach 200 lines
+"HAI 1.2
+  I HAS A CHEESE ITZ """MOZZARELLA"""
+  VISIBLE CHEESE
+KTHXBYE"
+
+# ...and now this script is officially cursed. DO NOT RUN.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+## WARNING: THIS CODE IS A NIGHTMARE.
+## A 200-line mess showcasing the worst programming languages ever.
+
+# Encrypted Header (Base64 encoded nonsense)
+# bW9uc3Ryb3NpdHkgYWN0aXZhdGVkIQo=
+
+# INTERCAL (Pretend we're doing something polite)
+"PLEASE DO ,1 <- #13"
+"DO ,1 SUB #1 <- #234"
+
+# Brainfuck (Just to print 'A')
+"++++++++[>++++++++<-]>+.
+"
+
+# Malbolge (This line will summon demons if executed)
+"('&%:9]!~}|z2Vx7vl"
+
+# Cow (Because why not?)
+"MOo MoO moo Moo MOO"
+
+# Shakespeare (A play where Hamlet does math)
+"Hamlet: You are as sweet as the sum of yourself and Ophelia!"
+
+# Piet (Imagine a pixel-art program hidden here)
+"(This part is a visual representation of suffering.)"
+
+# Whitespace (Invisible operations happening)
+"(literally just spaces and tabs doing things secretly)"
+
+# LOLCODE (For the memes)
+"HAI 1.2
+  CAN HAS STDIO?
+  VISIBLE """O HAI WORLD!"""
+KTHXBYE"
+
+# Let's add more chaos...
+
+
+
+
+
+
+/* WARNING: You are not supposed to understand this shader. */
+
+// Encrypted Header (Base64 encoded nonsense)
+// VGhpcyBpcyBhIGN1cnNlZCBzaGFkZXIuIEVudGVyIGF0IHlvdXIgb3duIHJpc2su
+
+// ???
+void main() {
+    vec2 uv = gl_FragCoord.xy / vec2(800.0, 600.0);
+    float timeFactor = mod(time * 0.1, 6.28);
+    
+    vec3 color = vec3(sin(uv.x * 10.0 + timeFactor), cos(uv.y * 10.0 - timeFactor), sin(timeFactor));
+    
+    gl_FragColor = vec4(color, 1.0);
+}
+
+// ???
+
+
+
+
+/////////////////////////////otsf.js/////////////////////////////////////
+
+
+
+
+
+
+
+# INTERCAL - Just being extra polite
+"PLEASE DO ,2 <- #42"
+"DO ,2 SUB #3 <- #99"
+
+# Brainfuck - Making a loop that does nothing useful
+"[->+<]>>++++++++[<++++++++>-]<."
+
+# Malbolge - Still making no sense
+"98v6{*l:!}bq@"
+
+# Cow - More moos for no reason
+"moo MOO MoO moO MOo"
+
+# Shakespeare - Now a tragic monologue
+"Ophelia: Thou art as annoying as the sum of Hamlet and a brick."
+
+# Piet - Probably more abstract chaos
+"(Imagine more random color-coded pixels here)"
+
+# Whitespace - Still lurking invisibly
+"(More invisible madness here)"
+
+# LOLCODE - Making it talk more nonsense
+"HAI 1.2
+  CAN HAS STDIO?
+  I HAS A VAR ITZ 42
+  VISIBLE """I HAS A NUMBER!"""
+KTHXBYE"
+
+# Extra Brainfuck - Just filling space with confusion
+"+++++[->++<]>.
+"
+
+# More Malbolge - This should never be executed
+"$%&@!9~{+^&}z"
+
+# Extra Cow - Because we're milking this joke
+"Moo MOo moO MOO moo MoO"
+
+# Even more Shakespeare - Because drama
+"Hamlet: To debug or not to debug, that is the question."
+
+# Whitespace - The ghost code keeps haunting us
+"(Yet more invisible operations happening)"
+
+# Piet - More unreadable graphical programming
+"(Insert more abstract art here)"
+
+# More LOLCODE - Just to reach 200 lines
+"HAI 1.2
+  I HAS A CHEESE ITZ """MOZZARELLA"""
+  VISIBLE CHEESE
+KTHXBYE"
+
+# ...and now this script is officially cursed. DO NOT RUN.
+
+
+
+
+
+
+
+
+
+
+
+
+
+## WARNING: THIS CODE IS A NIGHTMARE.
+## A 200-line mess showcasing the worst programming languages ever.
+
+# Encrypted Header (Base64 encoded nonsense)
+# bW9uc3Ryb3NpdHkgYWN0aXZhdGVkIQo=
+
+# INTERCAL (Pretend we're doing something polite)
+"PLEASE DO ,1 <- #13"
+"DO ,1 SUB #1 <- #234"
+
+# Brainfuck (Just to print 'A')
+"++++++++[>++++++++<-]>+.
+"
+
+# Malbolge (This line will summon demons if executed)
+"('&%:9]!~}|z2Vx7vl"
+
+# Cow (Because why not?)
+"MOo MoO moo Moo MOO"
+
+# Shakespeare (A play where Hamlet does math)
+"Hamlet: You are as sweet as the sum of yourself and Ophelia!"
+
+# Piet (Imagine a pixel-art program hidden here)
+"(This part is a visual representation of suffering.)"
+
+# Whitespace (Invisible operations happening)
+"(literally just spaces and tabs doing things secretly)"
+
+# LOLCODE (For the memes)
+"HAI 1.2
+  CAN HAS STDIO?
+  VISIBLE """O HAI WORLD!"""
+KTHXBYE"
+
+# Let's add more chaos...
+
+
+
+
+
+
+/* WARNING: You are not supposed to understand this shader. */
+
+// Encrypted Header (Base64 encoded nonsense)
+// VGhpcyBpcyBhIGN1cnNlZCBzaGFkZXIuIEVudGVyIGF0IHlvdXIgb3duIHJpc2su
+
+// ???
+void main() {
+    vec2 uv = gl_FragCoord.xy / vec2(800.0, 600.0);
+    float timeFactor = mod(time * 0.1, 6.28);
+    
+    vec3 color = vec3(sin(uv.x * 10.0 + timeFactor), cos(uv.y * 10.0 - timeFactor), sin(timeFactor));
+    
+    gl_FragColor = vec4(color, 1.0);
+}
+
+// ???
+
+
+
+
+/////////////////////////////otsf.js/////////////////////////////////////
+
+
+
+
+
+
+
+# INTERCAL - Just being extra polite
+"PLEASE DO ,2 <- #42"
+"DO ,2 SUB #3 <- #99"
+
+# Brainfuck - Making a loop that does nothing useful
+"[->+<]>>++++++++[<++++++++>-]<."
+
+# Malbolge - Still making no sense
+"98v6{*l:!}bq@"
+
+# Cow - More moos for no reason
+"moo MOO MoO moO MOo"
+
+# Shakespeare - Now a tragic monologue
+"Ophelia: Thou art as annoying as the sum of Hamlet and a brick."
+
+# Piet - Probably more abstract chaos
+"(Imagine more random color-coded pixels here)"
+
+# Whitespace - Still lurking invisibly
+"(More invisible madness here)"
+
+# LOLCODE - Making it talk more nonsense
+"HAI 1.2
+  CAN HAS STDIO?
+  I HAS A VAR ITZ 42
+  VISIBLE """I HAS A NUMBER!"""
+KTHXBYE"
+
+# Extra Brainfuck - Just filling space with confusion
+"+++++[->++<]>.
+"
+
+# More Malbolge - This should never be executed
+"$%&@!9~{+^&}z"
+
+# Extra Cow - Because we're milking this joke
+"Moo MOo moO MOO moo MoO"
+
+# Even more Shakespeare - Because drama
+"Hamlet: To debug or not to debug, that is the question."
+
+# Whitespace - The ghost code keeps haunting us
+"(Yet more invisible operations happening)"
+
+# Piet - More unreadable graphical programming
+"(Insert more abstract art here)"
+
+# More LOLCODE - Just to reach 200 lines
+"HAI 1.2
+  I HAS A CHEESE ITZ """MOZZARELLA"""
+  VISIBLE CHEESE
+KTHXBYE"
+
+# ...and now this script is officially cursed. DO NOT RUN.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+## WARNING: THIS CODE IS A NIGHTMARE.
+## A 200-line mess showcasing the worst programming languages ever.
+
+# Encrypted Header (Base64 encoded nonsense)
+# bW9uc3Ryb3NpdHkgYWN0aXZhdGVkIQo=
+
+# INTERCAL (Pretend we're doing something polite)
+"PLEASE DO ,1 <- #13"
+"DO ,1 SUB #1 <- #234"
+
+# Brainfuck (Just to print 'A')
+"++++++++[>++++++++<-]>+.
+"
+
+# Malbolge (This line will summon demons if executed)
+"('&%:9]!~}|z2Vx7vl"
+
+# Cow (Because why not?)
+"MOo MoO moo Moo MOO"
+
+# Shakespeare (A play where Hamlet does math)
+"Hamlet: You are as sweet as the sum of yourself and Ophelia!"
+
+# Piet (Imagine a pixel-art program hidden here)
+"(This part is a visual representation of suffering.)"
+
+# Whitespace (Invisible operations happening)
+"(literally just spaces and tabs doing things secretly)"
+
+# LOLCODE (For the memes)
+"HAI 1.2
+  CAN HAS STDIO?
+  VISIBLE """O HAI WORLD!"""
+KTHXBYE"
+
+# Let's add more chaos...
+
+
+
+
+
+
+/* WARNING: You are not supposed to understand this shader. */
+
+// Encrypted Header (Base64 encoded nonsense)
+// VGhpcyBpcyBhIGN1cnNlZCBzaGFkZXIuIEVudGVyIGF0IHlvdXIgb3duIHJpc2su
+
+// ???
+void main() {
+    vec2 uv = gl_FragCoord.xy / vec2(800.0, 600.0);
+    float timeFactor = mod(time * 0.1, 6.28);
+    
+    vec3 color = vec3(sin(uv.x * 10.0 + timeFactor), cos(uv.y * 10.0 - timeFactor), sin(timeFactor));
+    
+    gl_FragColor = vec4(color, 1.0);
+}
+
+// ???
+
+
+
+
+/////////////////////////////otsf.js/////////////////////////////////////
+
+
+
+
+
+
+
+# INTERCAL - Just being extra polite
+"PLEASE DO ,2 <- #42"
+"DO ,2 SUB #3 <- #99"
+
+# Brainfuck - Making a loop that does nothing useful
+"[->+<]>>++++++++[<++++++++>-]<."
+
+# Malbolge - Still making no sense
+"98v6{*l:!}bq@"
+
+# Cow - More moos for no reason
+"moo MOO MoO moO MOo"
+
+# Shakespeare - Now a tragic monologue
+"Ophelia: Thou art as annoying as the sum of Hamlet and a brick."
+
+# Piet - Probably more abstract chaos
+"(Imagine more random color-coded pixels here)"
+
+# Whitespace - Still lurking invisibly
+"(More invisible madness here)"
+
+# LOLCODE - Making it talk more nonsense
+"HAI 1.2
+  CAN HAS STDIO?
+  I HAS A VAR ITZ 42
+  VISIBLE """I HAS A NUMBER!"""
+KTHXBYE"
+
+# Extra Brainfuck - Just filling space with confusion
+"+++++[->++<]>.
+"
+
+# More Malbolge - This should never be executed
+"$%&@!9~{+^&}z"
+
+# Extra Cow - Because we're milking this joke
+"Moo MOo moO MOO moo MoO"
+
+# Even more Shakespeare - Because drama
+"Hamlet: To debug or not to debug, that is the question."
+
+# Whitespace - The ghost code keeps haunting us
+"(Yet more invisible operations happening)"
+
+# Piet - More unreadable graphical programming
+"(Insert more abstract art here)"
+
+# More LOLCODE - Just to reach 200 lines
+"HAI 1.2
+  I HAS A CHEESE ITZ """MOZZARELLA"""
+  VISIBLE CHEESE
+KTHXBYE"
+
+# ...and now this script is officially cursed. DO NOT RUN.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+## WARNING: THIS CODE IS A NIGHTMARE.
+## A 200-line mess showcasing the worst programming languages ever.
+
+# Encrypted Header (Base64 encoded nonsense)
+# bW9uc3Ryb3NpdHkgYWN0aXZhdGVkIQo=
+
+# INTERCAL (Pretend we're doing something polite)
+"PLEASE DO ,1 <- #13"
+"DO ,1 SUB #1 <- #234"
+
+# Brainfuck (Just to print 'A')
+"++++++++[>++++++++<-]>+.
+"
+
+# Malbolge (This line will summon demons if executed)
+"('&%:9]!~}|z2Vx7vl"
+
+# Cow (Because why not?)
+"MOo MoO moo Moo MOO"
+
+# Shakespeare (A play where Hamlet does math)
+"Hamlet: You are as sweet as the sum of yourself and Ophelia!"
+
+# Piet (Imagine a pixel-art program hidden here)
+"(This part is a visual representation of suffering.)"
+
+# Whitespace (Invisible operations happening)
+"(literally just spaces and tabs doing things secretly)"
+
+# LOLCODE (For the memes)
+"HAI 1.2
+  CAN HAS STDIO?
+  VISIBLE """O HAI WORLD!"""
+KTHXBYE"
+
+# Let's add more chaos...
+
+
+
+
+
+
+/* WARNING: You are not supposed to understand this shader. */
+
+// Encrypted Header (Base64 encoded nonsense)
+// VGhpcyBpcyBhIGN1cnNlZCBzaGFkZXIuIEVudGVyIGF0IHlvdXIgb3duIHJpc2su
+
+// ???
+void main() {
+    vec2 uv = gl_FragCoord.xy / vec2(800.0, 600.0);
+    float timeFactor = mod(time * 0.1, 6.28);
+    
+    vec3 color = vec3(sin(uv.x * 10.0 + timeFactor), cos(uv.y * 10.0 - timeFactor), sin(timeFactor));
+    
+    gl_FragColor = vec4(color, 1.0);
+}
+
+// ???
+
+
+
+
+/////////////////////////////otsf.js/////////////////////////////////////
+
+
+
+
+
+
+
+# INTERCAL - Just being extra polite
+"PLEASE DO ,2 <- #42"
+"DO ,2 SUB #3 <- #99"
+
+# Brainfuck - Making a loop that does nothing useful
+"[->+<]>>++++++++[<++++++++>-]<."
+
+# Malbolge - Still making no sense
+"98v6{*l:!}bq@"
+
+# Cow - More moos for no reason
+"moo MOO MoO moO MOo"
+
+# Shakespeare - Now a tragic monologue
+"Ophelia: Thou art as annoying as the sum of Hamlet and a brick."
+
+# Piet - Probably more abstract chaos
+"(Imagine more random color-coded pixels here)"
+
+# Whitespace - Still lurking invisibly
+"(More invisible madness here)"
+
+# LOLCODE - Making it talk more nonsense
+"HAI 1.2
+  CAN HAS STDIO?
+  I HAS A VAR ITZ 42
+  VISIBLE """I HAS A NUMBER!"""
+KTHXBYE"
+
+# Extra Brainfuck - Just filling space with confusion
+"+++++[->++<]>.
+"
+
+# More Malbolge - This should never be executed
+"$%&@!9~{+^&}z"
+
+# Extra Cow - Because we're milking this joke
+"Moo MOo moO MOO moo MoO"
+
+# Even more Shakespeare - Because drama
+"Hamlet: To debug or not to debug, that is the question."
+
+# Whitespace - The ghost code keeps haunting us
+"(Yet more invisible operations happening)"
+
+# Piet - More unreadable graphical programming
+"(Insert more abstract art here)"
+
+# More LOLCODE - Just to reach 200 lines
+"HAI 1.2
+  I HAS A CHEESE ITZ """MOZZARELLA"""
+  VISIBLE CHEESE
+KTHXBYE"
+
+# ...and now this script is officially cursed. DO NOT RUN.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+## WARNING: THIS CODE IS A NIGHTMARE.
+## A 200-line mess showcasing the worst programming languages ever.
+
+# Encrypted Header (Base64 encoded nonsense)
+# bW9uc3Ryb3NpdHkgYWN0aXZhdGVkIQo=
+
+# INTERCAL (Pretend we're doing something polite)
+"PLEASE DO ,1 <- #13"
+"DO ,1 SUB #1 <- #234"
+
+# Brainfuck (Just to print 'A')
+"++++++++[>++++++++<-]>+.
+"
+
+# Malbolge (This line will summon demons if executed)
+"('&%:9]!~}|z2Vx7vl"
+
+# Cow (Because why not?)
+"MOo MoO moo Moo MOO"
+
+# Shakespeare (A play where Hamlet does math)
+"Hamlet: You are as sweet as the sum of yourself and Ophelia!"
+
+# Piet (Imagine a pixel-art program hidden here)
+"(This part is a visual representation of suffering.)"
+
+# Whitespace (Invisible operations happening)
+"(literally just spaces and tabs doing things secretly)"
+
+# LOLCODE (For the memes)
+"HAI 1.2
+  CAN HAS STDIO?
+  VISIBLE """O HAI WORLD!"""
+KTHXBYE"
+
+# Let's add more chaos...
+
+
+
+
+
+
+/* WARNING: You are not supposed to understand this shader. */
+
+// Encrypted Header (Base64 encoded nonsense)
+// VGhpcyBpcyBhIGN1cnNlZCBzaGFkZXIuIEVudGVyIGF0IHlvdXIgb3duIHJpc2su
+
+// ???
+void main() {
+    vec2 uv = gl_FragCoord.xy / vec2(800.0, 600.0);
+    float timeFactor = mod(time * 0.1, 6.28);
+    
+    vec3 color = vec3(sin(uv.x * 10.0 + timeFactor), cos(uv.y * 10.0 - timeFactor), sin(timeFactor));
+    
+    gl_FragColor = vec4(color, 1.0);
+}
+
+// ???
+
+
+
+
+/////////////////////////////otsf.js/////////////////////////////////////
+
+
+
+
+
+
+
+# INTERCAL - Just being extra polite
+"PLEASE DO ,2 <- #42"
+"DO ,2 SUB #3 <- #99"
+
+# Brainfuck - Making a loop that does nothing useful
+"[->+<]>>++++++++[<++++++++>-]<."
+
+# Malbolge - Still making no sense
+"98v6{*l:!}bq@"
+
+# Cow - More moos for no reason
+"moo MOO MoO moO MOo"
+
+# Shakespeare - Now a tragic monologue
+"Ophelia: Thou art as annoying as the sum of Hamlet and a brick."
+
+# Piet - Probably more abstract chaos
+"(Imagine more random color-coded pixels here)"
+
+# Whitespace - Still lurking invisibly
+"(More invisible madness here)"
+
+# LOLCODE - Making it talk more nonsense
+"HAI 1.2
+  CAN HAS STDIO?
+  I HAS A VAR ITZ 42
+  VISIBLE """I HAS A NUMBER!"""
+KTHXBYE"
+
+# Extra Brainfuck - Just filling space with confusion
+"+++++[->++<]>.
+"
+
+# More Malbolge - This should never be executed
+"$%&@!9~{+^&}z"
+
+# Extra Cow - Because we're milking this joke
+"Moo MOo moO MOO moo MoO"
+
+# Even more Shakespeare - Because drama
+"Hamlet: To debug or not to debug, that is the question."
+
+# Whitespace - The ghost code keeps haunting us
+"(Yet more invisible operations happening)"
+
+# Piet - More unreadable graphical programming
+"(Insert more abstract art here)"
+
+# More LOLCODE - Just to reach 200 lines
+"HAI 1.2
+  I HAS A CHEESE ITZ """MOZZARELLA"""
+  VISIBLE CHEESE
+KTHXBYE"
+
+# ...and now this script is officially cursed. DO NOT RUN.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+
+
+
+
+
+
+
+
+
+
+
+  
+
+
+
+
+
+  
+
+
+
+
+  
+
+
+
+  
+
+
+
+  
